4c5621c842984e246d85d3083ea016c01283b85d
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2025-05-04 12:04:38 +0200

    // rest controller per la gestione centralizzata degli errori

--- src/main/java/com/example/demo/utility/ControllerResponseExceptions.java
+++ src/main/java/com/example/demo/utility/ControllerResponseExceptions.java
@@ -1,7 +1,6 @@
 package com.example.demo.utility;
 
 import com.example.demo.dto.responses.StringResponse;
-import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
