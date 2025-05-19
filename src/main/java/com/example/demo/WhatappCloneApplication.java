39f1aa154b4a8dd3973ad8f3b86ba7c5147c4320
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2025-05-19 11:53:03 +0200

    aggiunta file logging gitignore e implementazione swagger tramite dipendenza maven nel pom, aggiunta info progetto nel file main del progetto tramite annotation di openApi docs

--- src/main/java/com/example/demo/WhatappCloneApplication.java
+++ src/main/java/com/example/demo/WhatappCloneApplication.java
@@ -1,12 +1,40 @@
 package com.example.demo;
 
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.security.access.annotation.Secured;
+
 
 @SpringBootApplication
 @EnableScheduling
+@OpenAPIDefinition(
+        info = @Info(
+                title = "clone whatsapp",
+                description = "clone of the popular whatsapp service, using websocket for message services and google auth for login",
+                version = "1.0.0",
+                contact = @Contact(
+                        name = "Antonio Rizzuti",
+                        email = "antonio.rizzuti@hotmail.com",
+                        url = "https://github.com/antonio-bit-1109"
+                ),
+                license = @License(
+                        name = "",
+                        url = ""
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "",
+                url = ""
+        )
+
+)
 public class WhatappCloneApplication {
 
     public static void main(String[] args) {
