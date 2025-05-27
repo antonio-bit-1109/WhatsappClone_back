5e0e96ddc1b85a2f214af3f57871c14fb837c81a
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2025-05-27 18:18:35 +0200

    Refactor ChatControllerSocket to remove unused chatWebSocketService dependency

--- src/main/java/com/example/demo/controller/chatController/ChatControllerSocket.java
+++ src/main/java/com/example/demo/controller/chatController/ChatControllerSocket.java
@@ -2,13 +2,10 @@ package com.example.demo.controller.chatController;
 
 import com.example.demo.dto.requests.chatMessage.ChatMessageDTO;
 import com.example.demo.enums.BrokerDestinations;
-import com.example.demo.interfaces.IChatWebSocketService;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-
-import java.util.UUID;
 
 
 // controller per la gestione dei messaggi webSocket
@@ -16,11 +13,11 @@ import java.util.UUID;
 public class ChatControllerSocket {
 
     private final SimpMessagingTemplate messagingTemplate;
-    private final IChatWebSocketService chatWebSocketService;
 
-    public ChatControllerSocket(SimpMessagingTemplate simpMessagingTemplate, IChatWebSocketService chatWebSocketService) {
+    public ChatControllerSocket(
+            SimpMessagingTemplate simpMessagingTemplate
+    ) {
         this.messagingTemplate = simpMessagingTemplate;
-        this.chatWebSocketService = chatWebSocketService;
     }
 
     // messaggi inviati ad /app/send-private
@@ -31,8 +28,6 @@ public class ChatControllerSocket {
         // prendo il chatID dal payload e lo uso per trovare
         // il chat identity (UUID) per identificare/creare il canale a cui inviare il messaggio
         // sul socket
-//        UUID identity = this.chatWebSocketService.retriveUuidChat(message);
-
         // messaggio inviato tramite webSocket al canale che sarà "/send-private
         this.messagingTemplate.convertAndSend(BrokerDestinations.PRIVATE + message.getChatIdentity());
     }
